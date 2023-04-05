59d4475d0f0c14486e44b2a65a9d671c9af3779b
vigneshnv2802 <111960490+vigneshnv2802@users.noreply.github.com>
2023-04-05 15:39:45 +0530

    Update sales_calculate.py

--- sales_calculate.py
+++ sales_calculate.py
@@ -1,42 +1,44 @@
-import pandas as pd
-import numpy as np
-
 def sales_calculate(size_wt, trend_wt, sales_data, test_vector):
 
-  # Convert the DataFrame to a list
-  my_list = test_vector.values.tolist()[0]
-  my_list_series=pd.Series(my_list) 
+  # Convert the list to series
+  my_list_series=pd.Series(test_vector) 
 
-  column_count=len(sales_data.axes[1])
+  sales_data_copy = sales_data.copy()
 
-  # checking dimensions of both dataframe 
-  if (column_count- len(test_vector.axes[1])== 1):
+  row_count=len(sales_data_copy.axes[0])
+
+  column_count=len(sales_data_copy.axes[1])
+
+  checking dimensions of both dataframe 
+  if (column_count- len(test_vector)== 1):
       print("continue")
   else:
       print("error")
 
-  sales_data["SSE"]=np.sum( np.power(sales_data.iloc[:, 1:] - my_list,2), axis=1)
+  sales_data_copy["SSE"]=np.sum( np.power(sales_data_copy.iloc[:, 1:] - test_vector,2), axis=1)
 
   # Correlation
-  sales_data["Correlation"]=''
+  sales_data_copy["Correlation"]=''
   for i in range(0,row_count):
-      sales_data_list = sales_data.iloc[i,1:column_count].tolist()
+      sales_data_list = sales_data_copy.iloc[i,1:column_count].tolist()
       test_vector = my_list_series.tolist()
       corr = pd.Series(sales_data_list).corr(pd.Series(test_vector))
-      sales_data.iloc[i, column_count+1] = -corr
+      sales_data_copy.iloc[i, column_count+1] = -corr
 
 
   # Ranking 
-  sales_data['SSE.rank'] = sales_data['SSE'].rank(method='first', na_option='keep')
-  sales_data['Correlation.rank'] = sales_data['Correlation'].rank(method='first', na_option='keep')
-  sales_data['final_rank'] =(sales_data['SSE.rank'] *size_wt)+(sales_data['Correlation.rank'] *trend_wt)
+  sales_data_copy['SSE.rank'] = sales_data_copy['SSE'].rank(method='first')
+  sales_data_copy['Correlation.rank'] = sales_data_copy['Correlation'].rank(method='first')
+  sales_data_copy['final_rank'] =(sales_data_copy['SSE.rank'] *size_wt)+(sales_data_copy['Correlation.rank'] *trend_wt)
 
-  return sales_data
-
+  return sales_data_copy
 
 # sales_data = pd.read_excel("unit_sales_file.xlsx")
-# test_vector = pd.read_excel('test_vector_file.xlsx',header = None)
-# size_wt=0.25
-# trend_wt=0.35
+# print(sales_data)
+# test_vector = [1030, 842, 913, 925, 972]
+# print(test_vector)
+# trend_wt = 0.05
+# size_wt = 0.25
+# # test_vector = pd.DataFrame(test_v, columns = None)
 
 # print(sales_calculate(size_wt, trend_wt, sales_data, test_vector))
